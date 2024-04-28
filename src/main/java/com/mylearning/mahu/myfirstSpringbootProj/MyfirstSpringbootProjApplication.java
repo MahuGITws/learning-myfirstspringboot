ca2262044bd4877cf45d46d1c3d8d74b835dfe51
mahu <mahudesh@gmail.com>
2024-04-28 19:58:21 +0100

    basicsetUp

---
+++ src/main/java/com/mylearning/mahu/myfirstSpringbootProj/MyfirstSpringbootProjApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.mahu.myfirstSpringbootProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstSpringbootProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstSpringbootProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mylearning/mahu/myfirstSpringbootProj/MyfirstSpringbootProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.mahu.myfirstSpringbootProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstSpringbootProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
